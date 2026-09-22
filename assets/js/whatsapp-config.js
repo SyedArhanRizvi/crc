5b3b08c30c1cac3949df2a044f5929b0846b607e
SyedArhanRizvi <arhanrizvi9@gmail.com>
2026-09-22 23:47:25 +0530

    update mobile

--- assets/js/whatsapp-config.js
+++ assets/js/whatsapp-config.js
@@ -7,11 +7,11 @@
 
   const cmsConfig = {
     projectName: "Godrej Arden Sigma 3 Greater Noida (NCR)",
-    phoneNumber: "9667571967",
-    contactNumber: "9667571967",
+    phoneNumber: "919667571967",
+    contactNumber: "919667571967",
     visibility: {"nonRera":{"whatsapp":true,"chatbot":true,"contact":true},"nriNonRera":{"whatsapp":false,"chatbot":true,"contact":false},"onRera":{"whatsapp":true,"chatbot":false,"contact":false},"nriRera":{"whatsapp":true,"chatbot":false,"contact":false}},
     chatbot: {
-      phoneNumber: "9667571967",
+      phoneNumber: "919667571967",
       message:
         "Hey There, I would like to explore further details About Godrej Arden Sigma 3 Greater Noida (NCR). Please Share Details.",
     },
